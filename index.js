e142922297a32216c351b799efc795aeec785d3c
kazuyaseki <track613armory@gmail.com>
2019-12-15 10:52:22 +0900

    split functions to make it more readable

--- index.js
+++ index.js
@@ -3,83 +3,105 @@ const numberConstraints = ['min', 'max'];
 
 module.exports = {
   plugin: schema => {
-    const inputFieldAstNodes = [];
-    for (let key in schema._typeMap) {
-      const node = schema._typeMap[key];
+    const inputFields = getNodesWithConstraintDirective(schema);
 
-      if (node.astNode && node.astNode.kind === 'InputObjectTypeDefinition') {
-        inputFieldAstNodes.push(node.astNode);
-      }
-    }
-
-    // filter nodes with only constraint directive
-    const nodes = inputFieldAstNodes.filter(node => {
-      let hasConstraint = false;
-
-      node.fields.forEach(field => {
-        field.directives.forEach(directive => {
-          if (directive.name.value === 'constraint') {
-            hasConstraint = true;
-          }
-        });
-      });
-
-      return hasConstraint;
-    });
-
-    const result = nodes
+    const result = inputFields
       .map(node => {
         // build string of yup object for each field
-        const fieldObjs = node.fields
-          .map(field => {
-            const constraints = field.directives
-              .filter(directive => directive.name.value === 'constraint')
-              .reduce((prev, current) => {
-                return [...prev, ...(current ? current.arguments : [])];
-              }, []);
-
-            if (constraints.length < 1) {
-              return '';
-            }
-
-            const hasStringContraint =
-              constraints.filter(constraint =>
-                stringConstraints.includes(constraint.name.value)
-              ).length > 0;
-            const hasNumberContraint =
-              constraints.filter(constraint =>
-                numberConstraints.includes(constraint.name.value)
-              ).length > 0;
-
-            if (hasStringContraint && hasNumberContraint) {
-              throw Error(
-                `directive of ${field.name.value} includes both string and number constraint`
-              );
-            }
-
-            return `${field.name.value}: yup${
-              hasStringContraint ? '.string()' : ''
-            }${hasNumberContraint ? '.number()' : ''}${constraints
-              .map(
-                constraint =>
-                  `.${getYupPropName(constraint.name.value)}(${
-                    constraint.value.value
-                  })`
-              )
-              .join('')}`;
-          })
+        const fieldYupObjectStrings = node.fields
+          .map(buildYupObjectStringByField)
           .filter(str => str.length > 0);
 
         return `export const ${
           node.name.value
-        }ValidationSchema = yup.object().shape({ ${fieldObjs.join(',\n')}})`;
+        }ValidationSchema = yup.object().shape({ ${fieldYupObjectStrings.join(
+          ',\n'
+        )}})`;
       })
-      .join('');
+      .join('\n\n');
 
     return `import * as yup from 'yup'
     ${result}`;
   }
 };
+
+function getNodesWithConstraintDirective(schema) {
+  // filter only input object
+  const inputFieldAstNodes = [];
+  for (let key in schema._typeMap) {
+    const node = schema._typeMap[key];
+
+    if (node.astNode && node.astNode.kind === 'InputObjectTypeDefinition') {
+      inputFieldAstNodes.push(node.astNode);
+    }
+  }
+
+  // filter input objects with constraint directive
+  return inputFieldAstNodes.filter(node => {
+    let hasConstraint = false;
+
+    node.fields.forEach(field => {
+      field.directives.forEach(directive => {
+        if (directive.name.value === 'constraint') {
+          hasConstraint = true;
+        }
+      });
+    });
+
+    return hasConstraint;
+  });
+}
+
+function getPropertyTypeString(constraints, fieldName) {
+  const hasStringContraint =
+    constraints.filter(constraint =>
+      stringConstraints.includes(constraint.name.value)
+    ).length > 0;
+  const hasNumberContraint =
+    constraints.filter(constraint =>
+      numberConstraints.includes(constraint.name.value)
+    ).length > 0;
+
+  if (hasStringContraint && hasNumberContraint) {
+    throw Error(
+      `directive of ${fieldName} includes both string and number constraint`
+    );
+  }
+
+  if (hasStringContraint) {
+    return '.string()';
+  }
+  if (hasNumberContraint) {
+    return '.number()';
+  }
+
+  throw Error(
+    `directive of ${fieldName} includes both string and number constraint`
+  );
+}
+
+function buildYupObjectStringByField(field) {
+  const constraints = field.directives
+    .filter(directive => directive.name.value === 'constraint')
+    .reduce((prev, current) => {
+      return [...prev, ...(current ? current.arguments : [])];
+    }, []);
+
+  if (constraints.length < 1) {
+    return '';
+  }
+
+  const fieldName = field.name.value;
+
+  const propTypeString = getPropertyTypeString(constraints, fieldName);
+
+  return `${fieldName}: yup${propTypeString}${constraints
+    .map(
+      constraint =>
+        `.${getYupPropName(constraint.name.value)}(${constraint.value.value})`
+    )
+    .join('')}`;
+}
 
 function getYupPropName(constraintName) {
   if (constraintName === 'minLength') {
